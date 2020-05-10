9a27f0ef98750b14b8c1b7231ee90cd05ab325a3
成宝 <63845081+jiachengii@users.noreply.github.com>
2020-05-10 18:59:56 +0800

    流行语网
    流行语

---
+++ pop/js/publics.js
@@ -0,0 +1,32 @@
+﻿$(function (){
+	//了解更多的悬浮提示
+    $("[data-toggle='popover']").popover();
+});
+function Toast(msg,duration){
+      duration=isNaN(duration)?3000:duration;
+      var m = document.createElement('div');
+      m.innerHTML = msg;
+      m.style.cssText="max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
+      document.body.appendChild(m);
+      setTimeout(function() {
+        var d = 0.5;
+        m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+        m.style.opacity = '0';
+        setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+      }, duration);
+    }
+	 //监听滚动
+ window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("top-btn").style.display = "block";
+    } else {
+        document.getElementById("top-btn").style.display = "none";
+    }
+}
+//返回顶部操作
+ function topk() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+	Toast('已回到顶部',2000);
+}
